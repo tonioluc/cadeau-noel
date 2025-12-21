4e12824ed76f87bd666bd278daeb0d4361efc6de
antonio <antoniorandria3342@gmail.com>
2025-12-21 21:17:32 +0300

    route to show login and register page

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 // Page accueil
 Route::get(
     '/',
     function () {
-        return view('welcome');
+        return redirect()->route('login.show');
     }
 )->name('home');
+
+// Auth pages (views only)
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login.show');
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register.show');
