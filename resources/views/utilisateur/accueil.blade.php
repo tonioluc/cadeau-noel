744cee77d0194de459d45169e71f22fe4976f59f
antonio <antoniorandria3342@gmail.com>
2025-12-24 08:57:39 +0300

    feat : add link to acces child number input

--- resources/views/utilisateur/accueil.blade.php
+++ resources/views/utilisateur/accueil.blade.php
@@ -11,7 +11,7 @@
     <h1>Bonjour <strong><?php echo e($utilisateur->nom); ?></strong></h1>
     <p>Solde actuel : <strong><?php echo e($utilisateur->solde); ?> Ar</strong></p>
     <p><a href="<?php echo e(route('depot.show')); ?>">Faire un dépôt</a></p>
-    <p><a href="<?php echo e(route('utilisateur.accueil')); ?>">Revenir à l'accueil utilisateur</a></p>
+    <p><a href="<?php echo e(route('utilisateur.form-entrer-nbr-enfants')); ?>">Générer des cadeaux</a></p>
     <form method="POST" action="<?php echo e(route('logout')); ?>" style="margin-top: 1rem;">
         <?php echo csrf_field(); ?>
         <button type="submit">Se déconnecter</button>
